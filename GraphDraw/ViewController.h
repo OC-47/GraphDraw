d0460ec651d14f6af3ec7c2e62567143d7c977b8
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-11-05 15:51:44 +0900

    clean.

--- GraphDraw/ViewController.h
+++ GraphDraw/ViewController.h
@@ -13,6 +13,7 @@
     
 @private CPTXYGraph *graph;
     
+    
 }
 
 
--- GraphDraw/ViewController.m
+++ GraphDraw/ViewController.m
@@ -19,18 +19,37 @@
     [super viewDidLoad];
     // Create graph from theme
 	graph = [[CPTXYGraph alloc] initWithFrame:CGRectZero];
-//	CPTTheme *theme = [CPTTheme themeNamed:kCPTDarkGradientTheme];
+    // テーマが変えられる
    	CPTTheme *theme = [CPTTheme themeNamed:kCPTPlainWhiteTheme];
 	[graph applyTheme:theme];
 	CPTGraphHostingView *hostingView = (CPTGraphHostingView *)self.view;
 	hostingView.collapsesLayers = NO; // Setting to YES reduces GPU memory usage, but can slow drawing/scrolling
 	hostingView.hostedGraph = graph;
     
-    
-    graph.plotAreaFrame.paddingLeft = 70.0;
+
+    // Frameの大きさ指定
+    graph.plotAreaFrame.paddingLeft = 30.0;
 	graph.plotAreaFrame.paddingTop = 20.0;
 	graph.plotAreaFrame.paddingRight = 20.0;
 	graph.plotAreaFrame.paddingBottom = 40.0;
+    
+    // X軸とY軸のメモリの幅と、中心位置の設定。
+    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *)graph.defaultPlotSpace;
+    plotSpace.xRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(0) length:CPTDecimalFromFloat(0.9)];
+    plotSpace.yRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(0) length:CPTDecimalFromFloat(0.9)];
+    
+    CPTLineStyle *lineStyle = [CPTLineStyle lineStyle];
+// －－－－－－－－－－　ここから再開　－－－－－－－－－－－
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
 @end
